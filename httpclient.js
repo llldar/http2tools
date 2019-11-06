9a7dfd1864693a2c10b00f5aac537eb203d16181
Nathaniel Lin <linlilulll@gmail.com>
2019-11-06 15:20:03 +0800

    improved error handling and url parsing

--- httpclient.js
+++ httpclient.js
@@ -1,7 +1,6 @@
 const http2 = require("http2");
 const { promisify } = require("util");
 const getLogger = require("./utils/logger");
-
 const logger = getLogger("http2Client");
 
 /**
@@ -18,10 +17,9 @@ const logger = getLogger("http2Client");
  */
 class HTTP2Client {
   static parseUrl(url) {
-    const regex = /(?<baseUrl>(https:\/\/)?[^/]+)\/?(?<path>.+)?/;
+    const regex = /^(?:(?<scheme>https?):\/\/)?(?<baseUrl>[^\/]+)(?:(?<!\/)\/(?!\/)(?<path>.*))?$/;
     const matchObj = regex.exec(url);
-    const { baseUrl, path } = matchObj.groups;
-    return { baseUrl, path };
+    return matchObj.groups;
   }
 
   static async get(url) {
@@ -50,73 +48,95 @@ class HTTP2Client {
    * @param {string} url
    * @param {string} method [GET, POST, PUT, PATCH, DELETE]
    * @param {object} body request body, will only be used when it's POST/PUT/PATCH
-   * @param {string} scheme header scheme, default to http
-   * @returns {JSON} response of the request, returns empty string "" on error
+   * @returns {JSON} response of the request, returns null on error
    */
-  static async request(url, method, body = null, scheme = "http") {
-    let { baseUrl, path } = HTTP2Client.parseUrl(url);
-    const data = [];
+  static async request(url, method, body = null) {
+    try {
+      let { scheme, baseUrl, path } = HTTP2Client.parseUrl(url);
+      const data = [];
 
-    const methodMap = {
-      GET: http2.constants.HTTP2_METHOD_GET,
-      POST: http2.constants.HTTP2_METHOD_POST,
-      PUT: http2.constants.HTTP2_METHOD_PUT,
-      PATCH: http2.constants.HTTP2_METHOD_PATCH,
-      DELETE: http2.constants.HTTP2_METHOD_DELETE
-    };
+      const methodMap = {
+        GET: http2.constants.HTTP2_METHOD_GET,
+        POST: http2.constants.HTTP2_METHOD_POST,
+        PUT: http2.constants.HTTP2_METHOD_PUT,
+        PATCH: http2.constants.HTTP2_METHOD_PATCH,
+        DELETE: http2.constants.HTTP2_METHOD_DELETE
+      };
 
-    if (!baseUrl || baseUrl.length === 0) {
-      logger.error(`Invalid url ${baseUrl}`);
-    } else {
-      if (!baseUrl.includes(scheme)) {
-        baseUrl = `${scheme}://${baseUrl}`;
-      }
-      if (!path) {
-        path = "";
-      }
-
-      const client = http2.connect(baseUrl);
-      let req = null;
-      if (["GET", "DELETE"].includes(method.toUpperCase())) {
-        req = client.request({
-          [http2.constants.HTTP2_HEADER_SCHEME]: scheme,
-          [http2.constants.HTTP2_HEADER_METHOD]:
-            methodMap[method.toUpperCase()],
-          [http2.constants.HTTP2_HEADER_PATH]: `/${path}`
-        });
-      } else if (["POST", "PATCH", "PUT"].includes(method.toUpperCase())) {
-        const buffer = Buffer.from(JSON.stringify(body));
-        req = client.request({
-          [http2.constants.HTTP2_HEADER_SCHEME]: scheme,
-          [http2.constants.HTTP2_HEADER_METHOD]:
-            methodMap[method.toUpperCase()],
-          [http2.constants.HTTP2_HEADER_PATH]: `/${path}`,
-          "Content-Type": "application/json",
-          "Content-Length": buffer.length
-        });
-        req.write(buffer);
+      if (!baseUrl || baseUrl.length === 0) {
+        logger.error(`Invalid url ${baseUrl}`);
       } else {
-        logger.error(
-          `Invalid method passed to http2Client, only supports ${Object.keys(
-            methodMap
-          ).join(", ")}`
-        );
-      }
+        if (!scheme) {
+          scheme = "https";
+        }
+        baseUrl = `${scheme}://${baseUrl}`;
+        if (!path) {
+          path = "";
+        }
 
-      if (req) {
-        req.setEncoding("utf8");
-
-        req.on("data", chunk => {
-          data.push(chunk);
+        const client = http2.connect(baseUrl);
+        client.setTimeout(100000);
+        client.on("timeout", () => {
+          throw new Error("HTTP2 Connection Timeout");
         });
 
-        req.end();
-        await promisify(req.on.bind(req))("end");
-      }
+        client.on("error", err => {
+          logger.error(err);
+        });
 
-      client.close();
+        let req = null;
+        if (["GET", "DELETE"].includes(method.toUpperCase())) {
+          req = client.request({
+            [http2.constants.HTTP2_HEADER_SCHEME]: scheme,
+            [http2.constants.HTTP2_HEADER_METHOD]:
+              methodMap[method.toUpperCase()],
+            [http2.constants.HTTP2_HEADER_PATH]: `/${path}`
+          });
+        } else if (["POST", "PATCH", "PUT"].includes(method.toUpperCase())) {
+          const buffer = Buffer.from(JSON.stringify(body));
+          req = client.request({
+            [http2.constants.HTTP2_HEADER_SCHEME]: scheme,
+            [http2.constants.HTTP2_HEADER_METHOD]:
+              methodMap[method.toUpperCase()],
+            [http2.constants.HTTP2_HEADER_PATH]: `/${path}`,
+            "Content-Type": "application/json",
+            "Content-Length": buffer.length
+          });
+          req.write(buffer);
+        } else {
+          logger.error(
+            `Invalid method passed to http2Client, only supports ${Object.keys(
+              methodMap
+            ).join(", ")}`
+          );
+        }
+
+        if (req) {
+          req.setEncoding("utf8");
+
+          req.on("data", chunk => {
+            data.push(chunk);
+          });
+
+          req.on("error", err => {
+            logger.error(err);
+          });
+
+          req.end();
+          await promisify(req.on.bind(req))("end");
+        }
+
+        client.close();
+      }
+      if (data && data.length > 0) {
+        return data.join();
+      } else {
+        return null;
+      }
+    } catch (err) {
+      logger.error(err);
+      return null;
     }
-    return data.join();
   }
 }
 
